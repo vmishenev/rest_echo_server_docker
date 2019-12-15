52935ec6058c757a2ec19cdaca59b1811ddc3780
vmishenev <vad-mishenev@yandex.ru>
2019-12-15 23:32:39 +0300

    edit server.py

--- server.py
+++ server.py
@@ -6,24 +6,17 @@ import redis
 import json
 import web
 
-client = MongoClient(host='mongodb', port=27017)
-db = client.mydb
-coll = db.mycoll
-
-cache = redis.Redis(host='rediska', port=6379)
-cache.ping()
-
 # For each end point there is a method that handles
 # it in the HandleRequest class
 
 urls = (r'/storage/?(?P<key>.+)?', 'HandleRequest')
 
 
-# This class is used to handle requests
+#
 
-class HandleRequest:
+class HandleRequestCntr:
 
-    # Initialization code
+    """This class is used to handle requests"""
 
     def __init__(self):
 
@@ -31,6 +24,7 @@ class HandleRequest:
         # encoding (utf8) for all requests
 
         web.header('Content-Type', 'text/json; charset=utf-8')
+        self.service = Service()
 
     # --------------------------------------------
 
@@ -40,27 +34,14 @@ class HandleRequest:
         if key is None:
             raise web.badrequest()
         else:
-
-            answ = cache.get(key)
+            answ = self.service.get(key)
             response = {}
             if answ == None:
-
-                db_doc = coll.find_one({'key': key})
-                if db_doc == None:
-                    response['Status'] = 'Not found'
-                    raise web.notfound(json.dumps(response))
-                else:
-                    response['message'] = db_doc['msg']
-                    response['Status'] = 'OK'
-                    cache.set(key, response['message'])  # update chache
+                response['Status'] = 'Not found'
+                raise web.notfound(json.dumps(response))
             else:
-
-                if isinstance(answ, bytes):
-                    response['message'] = answ.decode('utf-8')
-                else:
-                    response['message'] = answ
+                response['message'] = answ
                 response['Status'] = 'OK'
-
             return json.dumps(response)
 
     # --------------------------------------------
@@ -73,15 +54,11 @@ class HandleRequest:
         else:
             response = {}
             obj_data = json.loads(web.data())
-            db_doc = coll.find_one({'key': key})
-            if db_doc == None:
+            res = self.service.add(key, obj_data['message'])
+            if res:
                 response['Status'] = 'Created'
-                coll.insert_one({'key': key, 'msg': obj_data['message']})
             else:
                 response['Status'] = 'OK'
-                db_doc['msg'] = obj_data['message']
-                if cache.exists(key):
-                    cache.set(key, obj_data['message'])
             return json.dumps(response)
 
     # --------------------------------------------
@@ -94,11 +71,8 @@ class HandleRequest:
         else:
             response = {}
             obj_data = json.loads(web.data())
-            db_doc = coll.find_one({'key': key})
-            if db_doc != None:
-                db_doc['msg'] = obj_data['message']
-                if cache.exists(key):
-                    cache.set(key, obj_data['message'])
+            res = self.service.service.edit(key, obj_data['message'])
+            if res:
                 response['Status'] = 'OK'
             else:
                 response['Status'] = 'Not found'
@@ -114,10 +88,7 @@ class HandleRequest:
             raise web.badrequest()
         else:
             response = {}
-            db_doc = coll.find_one({'key': key})
-            if db_doc != None:
-                coll.remove(db_doc)
-                cache.delete(key)
+            if service.delete(key):
                 response['Status'] = 'OK'
             else:
                 response['Status'] = 'Not found'
@@ -126,6 +97,93 @@ class HandleRequest:
 
 
     # --------------------------------------------
+
+class Service:
+
+    def __init__(self):
+        self.rep = Repository()
+
+    def delete(self, key):
+        db_doc = self.rep.findDocDb(key)
+        if db_doc != None:
+            self.rep.removeFromDb(db_doc)
+            self.rep.removeFromChache(key)
+            return True
+        else:
+            return False
+
+    def add(self, key, msg):
+        db_doc = self.rep.findDocDb(key)
+        if db_doc == None:
+            self.rep.insertDb(key, msg)
+            return True  # added
+        else:
+            db_doc['msg'] = msg
+            if self.rep.findChache(key) != None:
+                self.rep.setChache(key, msg)
+            return False
+
+    def edit(self, key, msg):
+        db_doc = self.rep.findDocDb(key)
+        if db_doc == None:
+            return False  # added
+        else:
+            db_doc['msg'] = msg
+            if self.rep.findChache(key) != None:
+                self.rep.setChache(key, msg)
+            return True
+
+    def get(self, key):
+        msg = self.rep.findChache(key)
+        if msg == None:
+            db_doc = self.rep.findDocDb(key)
+            setChache(key, db_doc['msg'])  # upd chache
+            return db_doc['msg']  # added
+        else:
+            return msg
+
+
+class Repository:
+
+    def __init__(self):
+        self.client = MongoClient(host='mongodb', port=27017)
+        self.db = client.mydb
+        self.coll = db.mycoll
+
+        self.cache = redis.Redis(host='rediska', port=6379)
+        self.cache.ping()
+
+    def insertDb(key, msg):
+        if findDocDb(key) == None:
+            self.coll.insert_one({'key': key, 'msg': msg})
+            return True
+        return False
+
+    def setChache(key, msg):
+        return self.cache.set(key, msg)
+
+    def findChache(key):
+        answ = self.cache.get(key)
+        if isinstance(answ, bytes):
+            answ.decode('utf-8')
+        else:
+            return answ
+
+    def findDocDb(db_doc):
+        return self.coll.find_one({'key': key})
+
+    def removeFromDb(db_doc):
+        self.coll.remove(db_doc)
+
+    def removeFromChache(key):
+        self.cache.delete(key)
+
+    def removeFromDb(db_doc):
+        self.coll.remove(db_doc)
+
+    def removeFromChache(key):
+        self.cache.delete(key)
+
 
 # Start web server
 
